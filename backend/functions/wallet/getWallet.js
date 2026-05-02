ecc78b2354b7545a1a26b557100efe84f24ebfc5
Tom <marstetom@ecs.vuw.ac.nz>
2026-05-02 21:38:09 +1200

    Integration Finished...

--- backend/functions/wallet/getWallet.js
+++ backend/functions/wallet/getWallet.js
@@ -1,6 +1,6 @@
 const { app } = require("@azure/functions");
-const { ObjectId } = require("mongodb");
 const { getDb } = require("../../../data/db");
+const mongoose = require("mongoose");
 
 app.http("getWallet", {
   methods: ["GET"],
@@ -10,7 +10,9 @@ app.http("getWallet", {
     const { userId } = req.params;
 
     const db = await getDb();
-    const wallet = await db.collection("Wallet").findOne({ userId: userId });
+    const wallet = await db.collection("wallets").findOne({ 
+      userId: new mongoose.Types.ObjectId(userId)  // convert string to ObjectId
+    });
 
     if (!wallet) {
       return { status: 404, jsonBody: { error: "Wallet not found" } };
